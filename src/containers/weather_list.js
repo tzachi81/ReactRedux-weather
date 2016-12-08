21412f4dead00c7be0bba42ea96bb912b7fe3b92
tzachi81 <tzahih@gmail.com>
2016-12-08 17:10:40 +0200

    adding charts with react-sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData){
         const name = cityData.city.name;
-        console.dir(name);
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
+        console.log(temps);
 
         return(
             <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="orange" units="k"/></td>
+                <td><Chart data={humidities} color="yellow"units="hPa"/></td>
+                <td><Chart data={pressures} color="lightblue" units="%"/></td>
             </tr>
         );
     };
@@ -19,9 +27,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temprature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temprature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
